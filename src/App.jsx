e15ef9e091cb5e6de74867cdc13d06fce6f837de
Jagadeesh Sura <jagadeesh.sura1997@gmail.com>
2026-09-20 09:20:10 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import HomePage from './pages/HomePage.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 function App() {
 
   return (
     <>
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    
     </>
   )
 }
 
 export default App
+ 
